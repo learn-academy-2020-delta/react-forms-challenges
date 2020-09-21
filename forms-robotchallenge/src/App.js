6a5af7c2010f3e3ff27a51c69f0188bc6e6b766a
drewbrad4 <drewbrad4@gmail.com>
2020-09-21 11:57:32 -0700

    initial commit

---
+++ forms-robotchallenge/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+
+// 3 child components for each Robot
