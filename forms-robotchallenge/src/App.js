1c690863be8daaf8511a1ecd484285ed0ba0d5b2
drewbrad4 <drewbrad4@gmail.com>
2020-09-21 14:42:02 -0700

    finished robot challenge

--- forms-robotchallenge/src/App.js
+++ forms-robotchallenge/src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import GoodBot from './Components/GoodBot';
+import BadBot from './Components/BadBot';
+import UsupBot from './Components/UsupBot';
 
 export default class App extends Component {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  constructor(props){
+    super(props)
+    this.state = {
+      userInput: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ userInput: e.target.value})
+  }
+
+  render (){
+    let { userInput } = this.state
+    return (
+      <div>
+        <div id="title-box">
+          <h1>Robo-Answers</h1>
+          <input 
+          type="text"
+          value={userInput}
+          onChange={this.handleChange}
+          placeholder="Say Something!"
+          />
+        </div>
+        <GoodBot 
+          userInput={userInput}
+        />
+        <BadBot 
+          userInput={userInput}
+        />
+        <UsupBot 
+          userInput={userInput}
+        />
+      </div>
+    );
+  }
 }
 
 
